efeaaec9791915431dd19aca59afcb57d6bfc4f4
GeJF <48742860+GeJF@users.noreply.github.com>
2019-05-14 09:57:25 +0800

    Add files via upload

---
+++ week5/gejiangfan32.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env Python
+#思路： rank((X - Y)) / rank((X + Y)))
+# -*- coding:utf-8 -*-
+# author: gejf
+
+import time
+
+import numpy as np
+import pandas as pd
+from FactorModule.FactorBase import FactorBase
+from DataReaderModule.Constants import ALIAS_FIELDS as t
+
+class Factor(FactorBase):
+
+    def __init__(self):
+        super(Factor,self).__init__()
+        self.neutral = True
+        self.factorName = __name__.split('.')[-1]
+        self.needFields = [t.CLOSE, t.LOW]  # 设置需要的字段
+
+    def factor_definition(self):
+        """
+        收集派发指标
+        :return:
+        """
+        s = time.time()
+        needData = self.needData                                # 计算所需数据
+        
+        adjX = needData[t.CLOSE]
+        adjY = needData[t.LOW]
+        tmp1 = adjX - adjY
+        tmp2 = adjX + adjY
+        ret1 = self.calculator.Rank(tmp1)
+        ret2 = self.calculator.Rank(tmp2)
+
+        factor = ret1/ret2
+
+        print('factor {0} done with {1} seconds'.format(self.factorName, time.time() - s))
+        return factor
+
+    def run_factor(self):
+        self.run()
+
+
+
+fct = Factor()
+fct.run_factor()
